bd35bf2924de85e49c8cce517f48b2489f0d8c8e
Jojoe07 <109483495+Jojoe07@users.noreply.github.com>
2025-02-13 08:10:32 -0300

    Update Visão_atual.py

--- Visão_atual.py
+++ Visão_atual.py
@@ -6,31 +6,27 @@ st.subheader("Extrato")
 
 ("Jornada atual app")
 
-embed_code = """<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="200" height="450" src="https://embed.figma.com/proto/OYgx6p20H1Y0dnoGkCmESh/Hacka-Empresas?node-id=1-15&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A16&embed-host=share" allowfullscreen></iframe>"""
-st.components.html(embed_code,height=400)
+embed_code = """<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="200" height="450" src="https://embed.figma.com/proto/OYgx6p20H1Y0dnoGkCmESh/Hacka-Empresas?node-id=1-16&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A16&embed-host=share" allowfullscreen></iframe>"""
+st.components.v1.html(embed_code,height=400)
 
 ("Jornada atual bankline")
 
 embed_code = """<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="300" height="450" src="https://embed.figma.com/proto/3ANXNPKYyd3dwg8HRijMZ7/Hacka-desktop?node-id=1-2&starting-point-node-id=1%3A2&embed-host=share" allowfullscreen></iframe>"""
-st.components.v1.html(embed_code, height=400)
+st.components.v1.html(embed_code, height=450)
+
 
 tab1 , tab2 = st.tabs(["Visão para o cliente " , "Visão para o operador"])
 tab1.write("""
- # Visão do cliente
+ **Visão do cliente**
  - Esclarece muito bem os literais
  - Maior autonomia
            
            """)
 tab2.write("""
- # Visão do operador
+ **Visão do operador**
  - Visão limitada, precisa ir em várias rotas para localizar as informações
  - Em alguns casos precisa deduzir do que se refere os valores
 """)
-
-
-
-
-
 
 
 
