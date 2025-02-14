c2843f1131657168daedade21e29d0069c9f0dd7
Jojoe07 <109483495+Jojoe07@users.noreply.github.com>
2025-02-14 11:20:43 -0300

    Update Visão_Futuro.py

--- pages/Visão_Futuro.py
+++ pages/Visão_Futuro.py
@@ -4,5 +4,5 @@ st.title("Jornada do futuro")
 
 ("App do futuro")
 
-embed_code = """<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="400" height="450" src="https://embed.figma.com/proto/OYgx6p20H1Y0dnoGkCmESh/Hacka-Empresas?node-id=75-2&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=75%3A2&show-proto-sidebar=1&embed-host=share" allowfullscreen></iframe>"""
+embed_code = """<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="300" height="450" src="https://embed.figma.com/proto/OYgx6p20H1Y0dnoGkCmESh/Hacka-Empresas?node-id=75-2&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=75%3A2&show-proto-sidebar=1&embed-host=share" allowfullscreen></iframe>"""
 st.components.v1.html(embed_code, height=450)
