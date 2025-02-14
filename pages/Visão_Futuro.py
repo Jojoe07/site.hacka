a31eeb158cac08d91c17b2dd2f55c7a08c194716
Jojoe07 <109483495+Jojoe07@users.noreply.github.com>
2025-02-14 11:18:07 -0300

    Update Visão_Futuro.py

--- pages/Visão_Futuro.py
+++ pages/Visão_Futuro.py
@@ -1,5 +1,8 @@
 import streamlit as st
 st.title("Jornada do futuro")
 
-("Hub no app")
-st.image("pages/Frame 11.png")
+
+("App do futuro")
+
+embed_code = """<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="400" height="450" src="https://embed.figma.com/proto/OYgx6p20H1Y0dnoGkCmESh/Hacka-Empresas?node-id=75-2&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=75%3A2&show-proto-sidebar=1&embed-host=share" allowfullscreen></iframe>"""
+st.components.v1.html(embed_code, height=450)
